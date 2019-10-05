09735b87e3b7ae5b191bba678e66849ea7ecb8ea
Sachin Pant <sachinpant1326@gmail.com>
2019-10-06 00:32:40 +0530

    firebase connected

--- app/src/main/java/com/example/campusstore/MainActivity.java
+++ app/src/main/java/com/example/campusstore/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.campusstore;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
